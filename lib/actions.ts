820d6065d6da3359696829150e7c9c1a290b716c
maxnchief <logandogan@gmail.com>
2025-10-08 13:24:14 -0500

    Implement addTopic function to handle topic insertion and revalidation

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,1 +1,20 @@
-// Define your server actions here
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { insertTopic } from "./data";
+import { redirect } from "next/navigation";
+
+export async function addTopic(data: FormData) {
+  let topic;
+  try {
+    topic = await insertTopic({
+      title: data.get("title") as string,
+    });
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to add topic.");
+  } finally {
+    revalidatePath("/ui/topics/[id]", "page");
+    topic && redirect(`/ui/topics/${topic.id}`);
+  }
+}
