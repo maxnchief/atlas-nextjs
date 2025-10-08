41833594c1c4a6355c3eb5b8f1b365566689f3b7
maxnchief <logandogan@gmail.com>
2025-10-08 12:20:31 -0500

    Add loading simulation and loading component for topic pages

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,4 +1,7 @@
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
+  //Simulate Page loading
+  await new Promise((r) => setTimeout(r, 3000));
+
   return <div>Topic Page: {id}</div>;
 }
